1b92830431656226c24be9091a9110ada6cf712f
Scott Silliman <scott.t.silliman@gmail.com>
2016-10-12 17:15:32 -0400

    first param to constructor is now request rather than the value of request.session

--- namespaced_session/session.py
+++ namespaced_session/session.py
@@ -16,48 +16,48 @@ def stringify_keys(path):
     return [str(part) for part in path]
 
 class NamespacedSession(object):
-    def __init__(self, session, path):
+    def __init__(self, request, path):
         self._path = stringify_keys(path)
-        self._dct = session
-        self._value = None
+        self._request = request
+        # self._value = None
         self._initialize_path_if_not_exists()
 
     def _initialize_path_if_not_exists(self):
-        self._value = get_nested_default(self._dct, self._path)
-        self._dct.save()
+        self._value = get_nested_default(self._request.session, self._path)
+        self.save()
 
     def get_sess_info(self):
-        return self._value
+        return get_nested_default(self._request.session, self._path)
 
     def save(self):
-        self._dct.save()
+        self._request.session.modified = True
 
     def get(self, path):
         """returns value at path in nss"""
         pth = self._path[:]
         pth.extend(stringify_keys(path))
-        return get_nested_default(self._dct, pth)
+        return get_nested_default(self._request.session, pth)
 
     def set(self, path, value):
         """sets value at path within nss"""
         pth = self._path[:]
         pth.extend(stringify_keys(path))
-        set_nested(self._dct, pth, value)
-        self._value = get_nested_default(self._dct, self._path)
+        set_nested(self._request.session, pth, value)
+        # self._value = get_nested_default(self._dct, self._path)
         self.save()
 
     def update(self, value):
         """performs a dict update at base path of nss"""
-        orig = get_nested_default(self._dct, self._path)
+        orig = get_nested_default(self._request.session, self._path)
         orig.update(value)
-        set_nested(self._dct, self._path, orig)
-        self._value = get_nested_default(self._dct, self._path)
+        set_nested(self._request.session, self._path, orig)
+        # self._value = get_nested_default(self._session, self._path)
         self.save()
 
     def clear(self):
         """empties the dict stored in the nss"""
-        set_nested(self._dct, self._path, {})
-        self._value = get_nested_default(self._dct, self._path)
+        set_nested(self._request.session, self._path, {})
+        # self._value = get_nested_default(self._dct, self._path)
         self.save()
 
     def delete(self, path):
@@ -67,9 +67,9 @@ class NamespacedSession(object):
         if len(head):
             pth = self._path[:]
             pth.extend(stringify_keys(head))
-            del get_nested_default(self._dct, pth)[key]
+            del get_nested_default(self._request.session, pth)[key]
         else:
-            del get_nested_default(self._dct, self._path)[key]
+            del get_nested_default(self._request.session, self._path)[key]
         self.save()
 
     def destroy(self):
@@ -77,14 +77,16 @@ class NamespacedSession(object):
         the path into the session object until it finds a key with an non-
         empty dict
         """
-        self._value = None
-        del get_nested_default(self._dct, self._path[:-1])[self._path[-1]]
+        self.clear()
+        del get_nested_default(self._request.session,
+                               self._path[:-1])[self._path[-1]]
         self._path.pop()
         while len(self._path):
-            curr_val = get_nested_default(self._dct, self._path)
+            curr_val = get_nested_default(self._request.session, self._path)
             if not len(curr_val):
                 # found an empty dict. delete it
-                parent = get_nested_default(self._dct, self._path[:-1])
+                parent = get_nested_default(self._request.session,
+                                            self._path[:-1])
                 del parent[self._path[-1]]
                 # remote tail from path
                 self._path.pop()
