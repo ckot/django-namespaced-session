87246280444d620ef5270e63f7d13cb14d9c8a32
Scott Silliman <scott.t.silliman@gmail.com>
2016-10-29 15:37:12 -0400

    removed 'Private :: Do not Upload' from classifiers which was causing problems with certain combinations of python and django

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Private :: Do Not Upload'
+        'Programming Language :: Python :: 2.7'
     ],
 )
